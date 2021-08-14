34bcc0c07b45db791af48d012cddf3cac620e909
Ethan Mohan <ethannmohan@gmail.com>
2021-08-13 23:41:09 -0400

    added webhook service in app/services/ for async processing, took someone's auth implementationgit add .

--- app/workers/webhook_worker.rb
+++ app/workers/webhook_worker.rb
@@ -1,11 +1,8 @@
 class WebhookWorker
+
   include Sidekiq::Worker
-
   sidekiq_options retry: 10, dead: false
-  sidekiq_retry_in do |retry_count|
-    jitter = rand(30.seconds..10.minutes).to_i
 
-    (retry_count ** 5) + jitter
   end
 
   def perform(webhook_event_id)
@@ -21,7 +18,7 @@ class WebhookWorker
                    .headers(
                      'User-Agent' => 'rails_webhook_system/1.0',
                      'Content-Type' => 'application/json',
-                     'Authorization' =>  'Bearer  ' + ENV['AUTH TOKEN']
+                     'Authorization' =>  'Bearer <token>'
                      )
                    .post(
                      webhook_endpoint.url,
@@ -59,7 +56,7 @@ class WebhookWorker
   end
 
   class FailedRequestError < StandardError; end
-  end
+
 
 
 
